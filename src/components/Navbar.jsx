d26e7f217680c3fc97276ab8a383634d96b28206
vishalreddy1005 <vishalreddy1005@gmail.com>
2025-04-05 04:18:38 +0530

    navbar improved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = ({ onLoginClick, onSignupClick }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <nav className="bg-white shadow-md">
+    <nav className="fixed top-0 left-0 w-full shadow-md z-20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex">
